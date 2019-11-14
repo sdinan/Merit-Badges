34d2fb53f1c3be68f9b8da2fe51c6f484c214e19
sdinan <54862340+sdinan@users.noreply.github.com>
2019-11-14 15:03:43 -0500

    Create guess-validate.js

---
+++ Guess/guess-validate.js
@@ -0,0 +1,24 @@
+/* Guess Validator 
+*By Sean Dinan
+*/
+
+var answer = Math.floor(Math.random()*100)+1;
+alert(answer);
+var guess = 0;
+var turns = 0;
+while (guess != answer){
+	guess=prompt("Guess my number (1-100)");
+    if (guess =="q") break;
+    if (validator(guess)==true){	
+      turns++;
+      if(guess<answer) alert("too low");
+	    else if(guess>answer) alert("too high");
+    }
+  else alert("Invalid Guess retry");
+}
+if (guess == answer) alert("You got in "+turns+" turns");
+else alert("quitter");
+
+function validator(guess){
+  if(guess > && <101) return true;
+  else return false;
